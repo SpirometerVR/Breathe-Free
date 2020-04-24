86201686f01f33b98a8a5d2f54fb418d2e8f0cf5
naman4real <namanom.g97@gmail.com>
2020-04-24 13:23:17 -0700

    BreatheFree - V1

---
+++ Breathe-Free/Assets/Scripts/MenuAnimatorFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAnimatorFunctions : MonoBehaviour
+{
+	[SerializeField] MenuButtonController menuButtonController;
+	public bool disableOnce;
+	public int index;
+
+	void PlaySound(AudioClip whichSound){
+		if(!disableOnce && !testing.audio.isPlaying){
+			menuButtonController.audioSource.PlayOneShot (whichSound);
+		}else{
+			disableOnce = false;
+		}
+	}
+}	
