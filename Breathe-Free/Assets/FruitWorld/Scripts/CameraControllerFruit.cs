c72859a190e6bdfc5798cbe323e2646bbf47abfc
omrifried <omrifried@gmail.com>
2020-04-29 11:02:15 -0700

    Fixed VR issues in Fruit World

---
+++ Breathe-Free/Assets/FruitWorld/Scripts/CameraControllerFruit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerFruit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transfrom.position = new Vector3(1.7f, 0.69f, -12.63f);
+    }
+}
