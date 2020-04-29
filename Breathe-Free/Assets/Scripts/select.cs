0a59d6aa247009ec43b9452bd586ae5a03b53a0a
naman4real <namanom.g97@gmail.com>
2020-04-28 21:27:42 -0700

    Removed XR package

--- Breathe-Free/Assets/Scripts/select.cs
+++ Breathe-Free/Assets/Scripts/select.cs
@@ -37,67 +37,67 @@ public class select : MonoBehaviour
 
     void Update()
     {
-        dir = transform.forward;
-        Ray ray = new Ray(transform.position, dir);
+        //dir = transform.forward;
+        //Ray ray = new Ray(transform.position, dir);
 
-        if (tempDir != ray.direction)
-        {
-            fruitDistance = Mathf.Infinity;
-        }
-        tempDir = ray.direction;
-        foreach (GameObject targetFruit in fruits)
-        {
+        //if (tempDir != ray.direction)
+        //{
+        //    fruitDistance = Mathf.Infinity;
+        //}
+        //tempDir = ray.direction;
+        //foreach (GameObject targetFruit in fruits)
+        //{
             
             
-            float dist = Vector3.Cross(ray.direction, targetFruit.transform.position - ray.origin).magnitude;
-            if (dist < fruitDistance)
-            {
-                fruitDistance = dist;
-                go = targetFruit;
-                if (previousGo)
-                {
-                    previousGo.transform.GetChild(0).gameObject.SetActive(false);
+        //    float dist = Vector3.Cross(ray.direction, targetFruit.transform.position - ray.origin).magnitude;
+        //    if (dist < fruitDistance)
+        //    {
+        //        fruitDistance = dist;
+        //        go = targetFruit;
+        //        if (previousGo)
+        //        {
+        //            previousGo.transform.GetChild(0).gameObject.SetActive(false);
 
-                }
-                go.transform.GetChild(0).gameObject.SetActive(true);
-                previousGo = go;
-                Debug.Log(dist + "yoooo" + go);
+        //        }
+        //        go.transform.GetChild(0).gameObject.SetActive(true);
+        //        previousGo = go;
+        //        Debug.Log(dist + "yoooo" + go);
 
-            }
+        //    }
             
-        }
+        //}
     
 
         //Debug.DrawLine(ray.origin, myCamera.transform.position, Color.red, 2f);
         //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10, Color.red);
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.CompareTag("fruits"))
-    //    {
-    //        stay = true;
-    //        go = other.gameObject;
-    //    }
-    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("fruits"))
+        {
+            stay = true;
+            go = other.gameObject;
+        }
+    }
 
-    //private void OnTriggerStay(Collider other)
-    //{
-    //    if (other.gameObject.CompareTag("fruits"))
-    //    {
-    //        other.gameObject.transform.GetChild(0).gameObject.SetActive(true);         // glow of the fruit  
-    //    }
-    //}
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("fruits"))
+        {
+            other.gameObject.transform.GetChild(0).gameObject.SetActive(true);         // glow of the fruit  
+        }
+    }
 
 
 
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.gameObject.CompareTag("fruits"))
-    //    {
-    //        other.gameObject.transform.GetChild(0).gameObject.SetActive(false);       //glow of the fruit
-    //        stay = false;
-    //    }
-    //}
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("fruits"))
+        {
+            other.gameObject.transform.GetChild(0).gameObject.SetActive(false);       //glow of the fruit
+            stay = false;
+        }
+    }
 }
    
