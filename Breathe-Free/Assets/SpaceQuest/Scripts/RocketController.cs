3ab636690509ff4110bbb4cc42991bb90fa603bf
omrifried <omrifried@gmail.com>
2020-04-29 01:07:38 -0700

    Space Quest edits for the headset

--- Breathe-Free/Assets/SpaceQuest/Scripts/RocketController.cs
+++ Breathe-Free/Assets/SpaceQuest/Scripts/RocketController.cs
@@ -136,6 +136,7 @@ public class RocketController : MonoBehaviour
         {
             Debug.Log("...cycle counter");
             this.gState = gameState.gameOver;
+            gameOver = true;
         }
     }
 
