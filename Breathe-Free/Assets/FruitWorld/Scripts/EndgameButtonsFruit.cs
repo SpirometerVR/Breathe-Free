6fbd81b42c0b558ea55fbf60e9d6134ba51af9ef
omrifried <omrifried@gmail.com>
2020-04-30 15:29:01 -0700

    Scoreboard for FW

---
+++ Breathe-Free/Assets/FruitWorld/Scripts/EndgameButtonsFruit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class EndgameButtonsFruit : MonoBehaviour
+{
+    private GameObject game;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void buttonPress()
+    {
+        game = EventSystem.current.currentSelectedGameObject;
+        if (game.name == "RetryButton")
+        {
+            SceneManager.LoadScene("FruitWorld");
+        }
+        else
+        {
+            SceneManager.LoadScene("StartMenu");
+        }
+    }
+}
